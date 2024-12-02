c9b6c3d28474db5b9cd7b804b72baaf5d1f4a878
Minhh17 <nguyenhaiminhr@gmail.com>
2024-12-02 18:18:46 +0700

    Change record config to Json

--- ESCA/src/modules/audiorecording/audioconfig.cpp
+++ ESCA/src/modules/audiorecording/audioconfig.cpp
@@ -2,53 +2,72 @@
 
 AudioConfig::AudioConfig(QObject *parent) : QObject{parent}
 {
-    int savedDeviceIndex = m_settings.value("audio/device", 0).toInt();  // Load saved device index
-    QString savedDeviceName = m_settings.value("audio/deviceName", "default").toString();  // Load saved device name
+    QFile configFile("/home/haiminh/Desktop/ESCA_Qt/ESCA/database/recording_params.json");
 
-    m_nearistParams.clear();
-    m_nearistParams.append(savedDeviceIndex);
-    m_nearistParams.append(m_settings.value("audio/codec", 0).toInt());
-    m_nearistParams.append(m_settings.value("audio/sampleRate", 0).toInt());
-    m_nearistParams.append(m_settings.value("audio/channel", 0).toInt());
-    m_nearistParams.append(m_settings.value("audio/endian", 0).toInt());
-    m_nearistParams.append(m_settings.value("audio/sampleSize", 0).toInt());
-    m_nearistParams.append(m_settings.value("audio/duration", 0).toInt());
+    if (configFile.open(QIODevice::ReadOnly)) {
+        QByteArray data = configFile.readAll();
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
 
-    emit nearistParamsChanged();
+        if (jsonDoc.isObject()) {
+            QJsonObject json = jsonDoc.object();
+            int savedDeviceIndex = json.value("device").toInt();  // Load saved device index
+            QString savedDeviceName = json.value("deviceName").toString("default");  // Load saved device name
 
-    qInfo() << "m_nearistParams: "<<m_nearistParams;
+            // qDebug() << "audio: "<<savedDeviceName;
+            // int savedDeviceIndex = m_settings.value("audio/device", 0).toInt();
+            // QString savedDeviceName = m_settings.value("audio/deviceName", "default").toString();
 
-    for (auto &deviceInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioInput)) {
-        cpplistDevices.append(deviceInfo);
-        m_listDevices.append(deviceInfo.deviceName());
-        // emit listDevicesChanged();
-        // qDebug() << m_listDevices.length();
-    }
-    qDebug() << m_listDevices.length();
+            m_nearistParams.clear();
+            m_nearistParams.append(savedDeviceIndex);
+            m_nearistParams.append(json.value("codec").toInt());
+            m_nearistParams.append(json.value("sampleRate").toInt());
+            m_nearistParams.append(json.value("channel").toInt());
+            m_nearistParams.append(json.value("endian").toInt());
+            m_nearistParams.append(json.value("sampleSize").toInt());
+            m_nearistParams.append(json.value("duration").toInt());
 
-    for (auto &outputInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioOutput)) {
-        cpplistOutput.append(outputInfo);
-        m_listOutput.append(outputInfo.deviceName());
-        // qDebug() << deviceInfo.deviceName();
-    }
+            emit nearistParamsChanged();
 
-    // Check if the device index is valid and if the name matches the saved one
-    if (savedDeviceIndex >= 0 && savedDeviceIndex < cpplistDevices.size() &&
-        cpplistDevices[savedDeviceIndex].deviceName() == savedDeviceName) {
-        m_deviceInfo = cpplistDevices[savedDeviceIndex];
-    } else {
-        // If device index is invalid or device name doesn't match, find the device by name
-        bool found = false;
-        for (const auto &device : cpplistDevices) {
-            if (device.deviceName() == savedDeviceName) {
-                m_deviceInfo = device;
-                found = true;
-                break;
+            qInfo() << "m_nearistParams: "<<m_nearistParams;
+
+            for (auto &deviceInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioInput)) {
+                cpplistDevices.append(deviceInfo);
+                m_listDevices.append(deviceInfo.deviceName());
+                // emit listDevicesChanged();
+                // qDebug() << m_listDevices.length();
             }
+            qDebug() << m_listDevices.length();
+
+            for (auto &outputInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioOutput)) {
+                cpplistOutput.append(outputInfo);
+                m_listOutput.append(outputInfo.deviceName());
+                // qDebug() << deviceInfo.deviceName();
+            }
+
+            // Check if the device index is valid and if the name matches the saved one
+            if (savedDeviceIndex >= 0 && savedDeviceIndex < cpplistDevices.size() &&
+                cpplistDevices[savedDeviceIndex].deviceName() == savedDeviceName) {
+                m_deviceInfo = cpplistDevices[savedDeviceIndex];
+            } else {
+                // If device index is invalid or device name doesn't match, find the device by name
+                bool found = false;
+                for (const auto &device : cpplistDevices) {
+                    if (device.deviceName() == savedDeviceName) {
+                        m_deviceInfo = device;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    m_deviceInfo = cpplistDevices.first();  // Default to the first device if no match found
+                }
+            }
+        } else {
+            qWarning() << "Failed to load configuration from JSON.";
         }
-        if (!found) {
-            m_deviceInfo = cpplistDevices.first();  // Default to the first device if no match found
-        }
+        configFile.close();
+    } else {
+        qWarning() << "Failed to open configuration file.";
     }
 
     m_format.setCodec(m_deviceInfo.supportedCodecs().at(m_nearistParams[1]));
@@ -65,7 +84,7 @@ AudioConfig::AudioConfig(QObject *parent) : QObject{parent}
     setListEndianz(m_deviceInfo.supportedByteOrders());
     setListSampleSize(m_deviceInfo.supportedSampleSizes());
 
-    connect(QGuiApplication::instance(), &QGuiApplication::aboutToQuit, this, &AudioConfig::saveSettings);
+    connect(QGuiApplication::instance(), &QGuiApplication::aboutToQuit, this, &AudioConfig::saveSettings);  // lưu vào file json trước khi tắt app
 
     qInfo() << "Initialized m_nearistParams from settings:" << m_nearistParams << m_deviceInfo.supportedSampleSizes();
     qInfo() << "m_format: "<<m_format << m_format.sampleSize();
@@ -88,32 +107,27 @@ void AudioConfig::changeDevice(int idx)
 
     setListCodecs(m_deviceInfo.supportedCodecs());
     if (!m_deviceInfo.supportedCodecs().empty()) {
-        m_format.setCodec(m_deviceInfo.supportedCodecs().at(0));
-        // m_nearistParams[1] = 0;
+        m_format.setCodec(m_deviceInfo.supportedCodecs().at(0));        
     }
 
     setListSampleRate(m_deviceInfo.supportedSampleRates());
     if (!m_deviceInfo.supportedSampleRates().empty()) {
-        m_format.setSampleRate(m_deviceInfo.supportedSampleRates().at(0));
-        // m_nearistParams[2] = 0;
+        m_format.setSampleRate(m_deviceInfo.supportedSampleRates().at(0));        
     }
 
     setListChannel(m_deviceInfo.supportedChannelCounts());
     if (!m_deviceInfo.supportedChannelCounts().empty()) {
-        m_format.setChannelCount(m_deviceInfo.supportedChannelCounts().at(0));
-        // m_nearistParams[3] = 0;
+        m_format.setChannelCount(m_deviceInfo.supportedChannelCounts().at(0));        
     }
 
     setListEndianz(m_deviceInfo.supportedByteOrders());
     if (!m_deviceInfo.supportedByteOrders().empty()) {
-        m_format.setByteOrder(m_deviceInfo.supportedByteOrders().at(0));
-        // m_nearistParams[4] = 0;
+        m_format.setByteOrder(m_deviceInfo.supportedByteOrders().at(0));        
     }
 
     setListSampleSize(m_deviceInfo.supportedSampleSizes());
     if (!m_deviceInfo.supportedSampleSizes().empty()) {
         m_format.setSampleSize(m_deviceInfo.supportedSampleSizes().at(0));
-        // m_nearistParams[5] = 0;
     }
 }
 
@@ -237,21 +251,15 @@ void AudioConfig::saveConfig(int device, int codec, int sampleRate, int channel,
         m_nearistParams[5] = m_deviceInfo.supportedSampleSizes().indexOf(m_format.sampleSize());
         emit nearistParamsChanged();
     }
-
     // saveSettings();
-
-    // QString iniFilePath = m_settings.fileName();
-    // qDebug() << "INI File Path:" << iniFilePath;
-    // qInfo()<< "save params c++: " << m_nearistParams;
-    // qInfo()<< "is save params true: " << m_deviceInfo.isFormatSupported(m_format);
 }
 
-QAudioDeviceInfo AudioConfig::deviceInfo() /*const*/
+QAudioDeviceInfo AudioConfig::deviceInfo()
 {
     return m_deviceInfo;
 }
 
-QAudioFormat AudioConfig::format() /*const*/
+QAudioFormat AudioConfig::format()
 {
     return m_format;
 }
@@ -261,12 +269,12 @@ QString AudioConfig::duration()
     return listDuration().at(m_nearistParams[6]);
 }
 
-QList<int> AudioConfig::nearistParams() /*const*/
+QList<int> AudioConfig::nearistParams()
 {
     return m_nearistParams;
 }
 
-bool AudioConfig::saveDone() /*const*/
+bool AudioConfig::saveDone()
 {
     return m_saveDone;
 }
@@ -281,36 +289,29 @@ void AudioConfig::setSaveDone(bool newSaveDone)
 
 void AudioConfig::saveSettings()
 {
-    m_settings.setValue("audio/deviceName", m_deviceInfo.deviceName());
-    // Lưu các giá trị vào QSettings
-    m_settings.setValue("audio/device", m_nearistParams[0]);
-    m_settings.setValue("audio/codec", m_nearistParams[1]);
-    m_settings.setValue("audio/sampleRate", m_nearistParams[2]);
-    m_settings.setValue("audio/channel", m_nearistParams[3]);
-    m_settings.setValue("audio/endian", m_nearistParams[4]);
-    m_settings.setValue("audio/sampleSize", m_nearistParams[5]);
-    m_settings.setValue("audio/duration", m_nearistParams[6]);
+    QJsonObject json;
 
-    // vì tín hiệu aboutToQuit của QGuiApp khá ngắn nên có lẽ I/O chưa kịp luu file.
-    // Mặc định, QSettings lưu trữ các thay đổi trong bộ nhớ
-    // và chỉ ghi chúng vào tệp khi cần thiết
-    // (khi QSettings bị hủy, hoặc khi ứng dụng kết thúc). Điều này giúp cải thiện hiệu suất, nhưng đôi khi,
-    //  bạn muốn đảm bảo rằng các thay đổi được
-    //  ghi ngay lập tức để tránh mất dữ liệu khi ứng dụng bị tắt đột ngột
-    //  hoặc thời gian tắt không đủ để tự động ghi.
+    // Save all parameters to the JSON object
+    json["device"] = m_nearistParams[0];
+    json["codec"] = m_nearistParams[1];
+    json["sampleRate"] = m_nearistParams[2];
+    json["channel"] = m_nearistParams[3];
+    json["endian"] = m_nearistParams[4];
+    json["sampleSize"] = m_nearistParams[5];
+    json["duration"] = m_nearistParams[6];
+    json["deviceName"] = m_deviceInfo.deviceName();
 
-    //Phương thức sync() thực hiện điều này bằng cách:
+    QJsonDocument jsonDoc(json);
 
-    //Ép QSettings ghi mọi thay đổi trong bộ nhớ xuống tệp trên đĩa.
-    //Đảm bảo rằng tất cả dữ liệu đã được lưu vào tệp trước khi tiếp tục.
+    qDebug()<< json << "dev name: "<<json["deviceName"];
 
-    m_settings.sync();
-
-    if (m_settings.status() != QSettings::NoError) {
-        qWarning() << "Error occurred while syncing settings to file.";
-    } else {
+    QFile configFile("/home/haiminh/Desktop/ESCA_Qt/ESCA/database/recording_params.json");
+    if (configFile.open(QIODevice::WriteOnly)) {
+        configFile.write(jsonDoc.toJson());
+        configFile.close();
         qInfo() << "Settings saved successfully.";
+    } else {
+        qWarning() << "Failed to open config file for saving.";
     }
-    qInfo() << "Configuration saved on exit." << m_settings.value("audio/device") <<m_settings.value("audio/sampleSize");
 }
 
